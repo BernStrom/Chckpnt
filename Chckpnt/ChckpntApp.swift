d71f5961cfb4ce3ecfce42c683925f034c428ccf
Bern N <bernnystrom@gmail.com>
2023-12-25 01:23:07 +0800

    🎉 (new): Initial commit
    Project boilerplate before any initial setup.

---
+++ Chckpnt/ChckpntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChckpntApp.swift
+//  Chckpnt
+//
+//  Created by Bern N on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ChckpntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chckpnt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chckpnt
+//
+//  Created by Bern N on 12/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
