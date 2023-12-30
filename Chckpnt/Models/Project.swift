7f327f6b3c268c237f0b47108d2b6c5b92e84167
Bern N <bernnystrom@gmail.com>
2023-12-31 05:58:31 +0800

    ♻️ (refactor): Change project update list variable to "updates"

--- Chckpnt/Models/Project.swift
+++ Chckpnt/Models/Project.swift
@@ -16,7 +16,7 @@ class Project: Identifiable {
     var startDate = Date()
     var focus = ""
     @Relationship(deleteRule: .cascade, inverse: \ProjectUpdate.project)
-    var update = [ProjectUpdate]()
+    var updates = [ProjectUpdate]()
     
     init() {
         id = UUID().uuidString
