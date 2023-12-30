673891be23bf4e28ef7e2c58f0d546f75beb7224
Bern N <bernnystrom@gmail.com>
2023-12-31 06:02:20 +0800

    💄 (style): Add UI styling to project detail view

--- Chckpnt/Views/ProjectDetailView.swift
+++ Chckpnt/Views/ProjectDetailView.swift
@@ -12,17 +12,114 @@ struct ProjectDetailView: View {
     var project: Project
     
     var body: some View {
-        VStack {
-            Text(project.name)
-                .navigationBarBackButtonHidden()
+        ZStack {
+            // MARK: - Background Styles
+            LinearGradient(
+                colors: [Color("Navy"), Color("Sky Blue")],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
             
-            Button("Back") {
-                dismiss()
+            LinearGradient(
+                colors: [Color("Washed Blue").opacity(0), Color("Sky Blue")],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .frame(width: 1)
+            .padding(.leading, -150)
+            
+            VStack {
+                // MARK: - Header
+                VStack(alignment: .leading, spacing: 13) {
+                    Text(project.name)
+                        .font(.screenHeading)
+                    
+                    HStack(alignment: .center, spacing: 13) {
+                        Spacer()
+                        
+                        StatBubbleView(title: "Hours", stat: "290", leadingColor: Color("Navy"), trailingColor: Color("Sky Blue"))
+                        
+                        StatBubbleView(title: "Sessions", stat: "34", leadingColor: Color("Turtle Green"), trailingColor: Color("Lime"))
+                        
+                        StatBubbleView(title: "Updates", stat: "32", leadingColor: Color("Maroon"), trailingColor: Color("Fuschia"))
+                        
+                        StatBubbleView(title: "Wins", stat: "9", leadingColor: Color("Maroon"), trailingColor: Color("Olive"))
+                        
+                        Spacer()
+                    }
+                    
+                    Text("My current focus is...")
+                        .font(.featuredText)
+                    
+                    HStack {
+                        Image(systemName: "checkmark.square")
+                        Text("Design the new app")
+                            .font(.featuredText)
+                    }
+                    .padding(.leading)
+                }
+                .foregroundStyle(.white)
+                .padding()
+                .background {
+                    Color(.black)
+                        .opacity(0.7)
+                        .clipShape(.rect(bottomLeadingRadius: 15, bottomTrailingRadius: 15))
+                        .ignoresSafeArea()
+                }
+                // MARK: - Project Updates
+                ScrollView {
+                    VStack(spacing: 27) {
+                        ProjectUpdateView()
+                        ProjectUpdateView()
+                        ProjectUpdateView()
+                        ProjectUpdateView()
+                    }
+                    .padding()
+                    .padding(.bottom, 80)
+                }
+                .scrollIndicators(.hidden)
+            }
+            // MARK: - Footer Buttons
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Button {
+                        // TODO: - Add new project button functionality
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .foregroundStyle(.black)
+                                .frame(width: 65)
+                            
+                            Image("cross")
+                        }
+                    }
+                    .padding([.leading, .top])
+                    
+                    Spacer()
+                    
+                    Button("Back") {
+                        dismiss()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .foregroundStyle(.white)
+                    .tint(.black)
+                    .padding([.trailing, .top])
+                }
+                .background {
+                    Color(.black)
+                        .opacity(0.5)
+                        .clipShape(.rect(topLeadingRadius: 15, topTrailingRadius: 15))
+                        .ignoresSafeArea()
+                }
             }
         }
+        .navigationBarBackButtonHidden()
     }
 }
 
-#Preview {
-    ProjectDetailView(project: Project())
-}
+//#Preview {
+//    ProjectDetailView(project: Project())
+//}
