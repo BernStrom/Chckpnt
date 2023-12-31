a8a118e27a001ca8504ebb34c8fcc096be946e56
Bern N <bernnystrom@gmail.com>
2024-01-01 01:57:41 +0800

    ✨ (feat): Add view for editing project focus

---
+++ Chckpnt/Views/EditFocusView.swift
@@ -0,0 +1,50 @@
+//
+//  EditFocusView.swift
+//  Chckpnt
+//
+//  Created by Bern N on 1/1/24.
+//
+
+import SwiftUI
+
+struct EditFocusView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var focus = ""
+    
+    var project: Project
+ 
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading) {
+                Text("Edit Project Focus")
+                    .font(.bigHeadline)
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    TextField("Focus", text: $focus)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Button("Save") {
+                        // Save project focus
+                        project.focus = focus
+                        dismiss()
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.blue)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.top)
+        }
+    }
+}
+
+//#Preview {
+//    EditFocusView()
+//}
